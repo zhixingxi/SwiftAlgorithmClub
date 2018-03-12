6c475c8c903acddf1f0ae342a5b2c500ba3c6b43
孟庆岭 <m276675358@163.com>
2018-03-12 14:02:41 +0800

    队列 数据结构

---
+++ SwiftAlgorithmClub.playground/Pages/QueueDataStructure.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+//: [Previous](@previous)
+//: 队列 数据结构
+
+public struct Queue<T> {
+    private var list = LinkedList<T>()
+    public func enqueue(_ element: T) {
+        list.append(value: element)
+    }
+    
+    public func dequeue() -> T? {
+        guard list.isEmpty, let element = list.first else {
+            return nil
+        }
+        list.remove(node: element)
+        return element.value
+    }
+    
+    public func peak() -> T? {
+        return list.first?.value
+    }
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
+    }
+}
+
+extension Queue: CustomStringConvertible {
+    // 2
+    public var description: String {
+        // 3
+        return list.description
+    }
+}
+
+var queue = Queue<Int>()
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(1)
+print(queue)
+var queueS = Queue<String>()
+queueS.enqueue("21sfdaf")
+queueS.enqueue("31sfad")
+queueS.enqueue("11dsfdfs")
+print(queueS)
+
+
+
